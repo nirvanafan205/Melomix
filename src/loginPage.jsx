59382fd98975ddbc108601e9295aa01cdea223db
captain.peanutbutter495 <47827017+nirvanafan205@users.noreply.github.com>
2023-11-06 01:30:11 -0800

    added more styles and navigation to home page

--- src/loginPage.jsx
+++ src/loginPage.jsx
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faLock, faUnlock } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUser,
+  faLock,
+  faUnlock,
+  faHouse,
+  faGears,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -17,9 +24,7 @@ const LoginPage = () => {
         <div className="absolute w-80 h-96 bg-gradient-to-r from-fuchsia-500 via-fuchsia-800 to-transparent -top-1/2 -left-1/2 animate-spin-slow origin-bottom-right bg-gradient-spin"></div>
 
         <div className="absolute inset-1 bg-gray-950 rounded-lg z-10 p-5">
-          <h1 className="text-4xl text-white font-bold text-center mb-6">
-            Login
-          </h1>
+          <h1 className="text-4xl text-center mb-6 animate-rainbow">Login</h1>
 
           <form action="">
             <div className="relative my-4">
@@ -31,7 +36,7 @@ const LoginPage = () => {
 
               <FontAwesomeIcon
                 icon={faUser}
-                className=" text-lg  absolute text-indigo-800 top-3 left-64"
+                className=" text-lg absolute text-indigo-800 top-3 left-64"
               />
 
               <label
@@ -64,6 +69,28 @@ const LoginPage = () => {
               >
                 Your Password
               </label>
+
+              {/* Takes you to registration page */}
+              <div className="mt-4">
+                <span className="text-xs text-pink-500 hover:text-cyan-300">
+                  New Here? Create an Account!
+                </span>
+              </div>
+            </div>
+
+            {/* Takes you to home page */}
+            <div className="flex justify-center mt-16 text-xs">
+              <Link to="/home">
+                <FontAwesomeIcon
+                  icon={faHouse}
+                  className="text-3xl text-teal-200 hover:text-violet-700"
+                />
+              </Link>
+
+              <FontAwesomeIcon
+                icon={faGears}
+                className="text-3xl text-emerald-300 ml-32 hover:text-cyan-400"
+              />
             </div>
           </form>
         </div>
