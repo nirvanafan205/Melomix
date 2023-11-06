b1914c3c9edc3defbd13b725031b1344f18094cb
captain.peanutbutter495 <47827017+nirvanafan205@users.noreply.github.com>
2023-11-05 23:00:44 -0800

    added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-// importing React and ReactDOM libraryies
-// BrowserRouter, Routes, and Route is used for client-side routing
-import ReactDOM from "react-dom"; // Correct the import statement
-import "./style.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./styles.css";
+import Login from "./loginPage";
 
-// serves as the root component of the application
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} /> {/* Define the route for the login page */}
+        {/* Add more routes for other pages if needed */}
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
-// Render the app component using ReactDOM.render
 ReactDOM.render(<App />, document.getElementById("root"));
