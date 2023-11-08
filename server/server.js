23e4c2446473199797c22a98bcbb0899d27ec243
nirvanafan205 <nirvanafan205@gmail.com>
2023-11-07 22:45:19 -0800

    dont merge, moving to laptop

--- server/server.js
+++ server/server.js
@@ -1,11 +1,32 @@
 const express = require("express");
+//const cors = require("cors");
+//const bodyParser = require("body-parser");
+const SpotifyWebApi = require("spotify-web-api-node");
+
 const app = express();
-const port = 3000;
+//app.use(cors());
+//app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.post("/login", (req, res) => {
+  const code = req.body.code;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:5173/search",
+    clientId: "5eb084a46a0a4ff9a4c885b1bdd130da",
+    clientSecret: "27188872cb704575b8a954349a48c27f",
+  });
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch(() => {
+      res.sendStatus(400);
+    });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
-});
+app.listen(5173);
