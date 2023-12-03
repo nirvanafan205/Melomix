7ddc7bc6505abaaccf4703360043b916b8382a48
ItsBrianUribe <brianuribe013@gmail.com>
2023-12-02 23:03:55 -0800

    Second version of the Navbar and Routers for updated page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,49 @@
 // importing React and ReactDOM libraryies
 // BrowserRouter, Routes, and Route is used for client-side routing
 import React from "react";
-import ReactDOM from "react-dom";
+import { Route, Routes } from 'react-router';
 import LandingPage from "./pages/landing-page/LandingPage";
 import Login from "./loginPage";
 import Registration from "./registrationPage";
 import Dashboard from "./components/Dashboard";
 import Settings from "./Settings";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 
 // serves as the root component of the application
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/registration" element={<Registration />} />
-        <Route path="dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
+function App(){
+  return(
+    <>
+    <div id="exclusive-container" class="container-fluid">
+      <div id="nav-row" class="row">
+        <div class="col-4">
+        </div>
+        <div class="col">
+          <Link to={"/"} className="nav-link">Home</Link>
+        </div>
+        <div class="col">
+          <Link to={"/dashboard"} className="nav-link">Dashboard</Link>
+        </div>
+        <div class="col">
+          <Link to={"/login"} className="nav-link">Login</Link>
+        </div>
+        <div class="col">
+          <Link to={"/settings"} className="nav-link">Settings</Link>
+        </div>
+        <div class="col-4">
+        </div>
+      </div>
+    </div>
 
-ReactDOM.render(<App />, document.getElementById("root"));
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="/registration" element={<Registration />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  </>
+  );
+}
+export default App;
