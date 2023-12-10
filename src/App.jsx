4522ca3c6da04a0df356ace512edc239b9e5857b
nirvanafan205 <nirvanafan205@gmail.com>
2023-12-09 21:18:48 -0800

    configured what is shown logged vs logged out

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,46 @@
-import React, { useEffect } from "react";
-import { Route, Routes, Link, useLocation } from "react-router-dom";
+import React, { useEffect, useContext } from "react";
+import {
+  Route,
+  Routes,
+  Link,
+  useLocation,
+  useNavigate,
+  Navigate,
+} from "react-router-dom";
 import LandingPage from "./pages/landing-page/LandingPage";
 import Login from "./loginPage";
 import Registration from "./registrationPage";
 import Dashboard from "./components/Dashboard";
 import Settings from "./Settings";
-import { Navigate } from "react-router-dom";
-import { useContext } from "react";
 import { UserContext } from "./UserContext";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 
 function App() {
   const location = useLocation();
-  const { user } = useContext(UserContext);
+  const navigate = useNavigate();
+  const { user, setUser } = useContext(UserContext);
+
   useEffect(() => {
     // Add a class to the body based on the active link
     document.body.className = location.pathname.replace("/", "") || "home";
   }, [location.pathname]);
 
-  // Check if the current path is one of the pages where the nav bar should not be shown
   const showNavBar = !["/login", "/registration", "/settings"].includes(
     location.pathname
   );
+
+  const handleLogout = () => {
+    setUser(null); // Clears the user context
+    navigate("/"); // Redirects to the login page
+  };
 
   return (
     <>
       {showNavBar && (
         <div id="exclusive-container" className="container-fluid">
-          <div id="nav-row" className="row">
-            <div className="col-4"></div>
-            <div className="col">
+          <div id="nav-row" className="row justify-content-center">
+            <div className="col-auto">
               <Link
                 to={"/"}
                 className={`nav-link ${
@@ -41,23 +51,39 @@ function App() {
               </Link>
             </div>
             {user && (
-              <div className="col">
-                <Link
-                  to={"/dashboard"}
-                  className={`nav-link ${
-                    location.pathname === "/dashboard" ? "active-link" : ""
-                  }`}
-                >
-                  Dashboard
-                </Link>
-              </div>
+              <>
+                <div className="col-auto">
+                  <Link
+                    to={"/dashboard"}
+                    className={`nav-link ${
+                      location.pathname === "/dashboard" ? "active-link" : ""
+                    }`}
+                  >
+                    Dashboard
+                  </Link>
+                </div>
+                <div className="col-auto">
+                  <span className="nav-link">{user.username}</span>
+                </div>
+                <div className="col-auto">
+                  <button onClick={handleLogout} className="logout-button btn">
+                    Logout
+                  </button>
+                </div>
+                <div className="col-auto">
+                  <Link
+                    to={"/settings"}
+                    className={`nav-link ${
+                      location.pathname === "/settings" ? "active-link" : ""
+                    }`}
+                  >
+                    Settings
+                  </Link>
+                </div>
+              </>
             )}
-            {user ? (
-              <div className="col">
-                <span className="nav-link">{user.username}</span>
-              </div>
-            ) : (
-              <div className="col">
+            {!user && (
+              <div className="col-auto">
                 <Link
                   to={"/login"}
                   className={`nav-link ${
@@ -68,29 +94,19 @@ function App() {
                 </Link>
               </div>
             )}
-            <div className="col">
-              <Link
-                to={"/settings"}
-                className={`nav-link ${
-                  location.pathname === "/settings" ? "active-link" : ""
-                }`}
-              >
-                Settings
-              </Link>
-            </div>
-            <div className="col-4"></div>
           </div>
         </div>
       )}
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/settings" element={<Settings />} />
         <Route path="/registration" element={<Registration />} />
         <Route
           path="/dashboard"
           element={user ? <Dashboard /> : <Navigate to="/login" />}
         />
+        {/* Only render the Settings route if the user is logged in */}
+        {user && <Route path="/settings" element={<Settings />} />}
       </Routes>
     </>
   );
