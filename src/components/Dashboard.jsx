8590478b3d5eeb6727d52eda52d5b898d389e0f4
thanhtttran <trantanthanh1101@gmail.com>
2023-12-12 12:19:39 -0800

    fixed lyrics fetch

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -37,7 +37,7 @@ export default function Dashboard() {
 
     try {
       //change this when deployed
-      const response = await fetch(`https://cool-azure-donut.glitch.me/scrape${encodedSongTitle}`);
+      const response = await fetch(`https://cool-azure-donut.glitch.me/scrape/${encodedSongTitle}`);
 
       if (!response.ok) {
         throw new Error(`Error: ${response.status}`);
