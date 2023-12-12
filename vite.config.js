624b33444312a1c0ef39ced55977ac84ae1c325b
thanhtttran <trantanthanh1101@gmail.com>
2023-12-12 07:29:55 -0800

    updated vite config js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: "src",
+  build: {
+    // The outDir option sets the output directory for the build
+    outDir: '../dist',
+    // If you have additional build options or requirements, set them here
+    // For example, setting emptyOutDir to false will prevent Vite from clearing the directory on each build
+    emptyOutDir: true,
+  },
 });
